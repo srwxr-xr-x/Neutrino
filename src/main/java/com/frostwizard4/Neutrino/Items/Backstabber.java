7c71052c979181b2aeb62259887714c387a17480
FrostWizard4 <88950263+FrostWizard4@users.noreply.github.com>
2021-08-31 12:17:54 -0500

    Update Backstabber.java

--- src/main/java/com/frostwizard4/Neutrino/Items/Backstabber.java
+++ src/main/java/com/frostwizard4/Neutrino/Items/Backstabber.java
@@ -9,10 +9,5 @@ public class Backstabber extends SwordItem {
     public Backstabber(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
     }
-    @Override
-    public boolean onClicked(net.minecraft.item.ItemStack stack, net.minecraft.item.ItemStack otherStack, net.minecraft.screen.slot.Slot slot, net.minecraft.util.ClickType clickType, net.minecraft.entity.player.PlayerEntity player, net.minecraft.inventory.StackReference cursorStackReference) {
-        player.playSound(HARVESTER_ACTIVATE, 1.0F, 1.0F);
-        return true;
-    }
 }
 
