017b36248b4f7a77c893c9c1161314123edb5fe2
FrostWizard4 <frostwizard4@gmail.com>
2021-09-08 19:07:36 -0500

    Update NeutrinoMain.java

--- src/main/java/com/frostwizard4/Neutrino/NeutrinoMain.java
+++ src/main/java/com/frostwizard4/Neutrino/NeutrinoMain.java
@@ -8,6 +8,8 @@ import com.frostwizard4.Neutrino.Items.DaggerToolMaterial;
 import com.frostwizard4.Neutrino.Items.GoatHorn;
 import com.frostwizard4.Neutrino.Slabs.CraftingSlab;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.OverworldBiomes;
+import net.fabricmc.fabric.api.biome.v1.OverworldClimate;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -16,10 +18,7 @@ import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Material;
-import net.minecraft.block.SlabBlock;
+import net.minecraft.block.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.entity.player.PlayerEntity;
@@ -30,21 +29,44 @@ import net.minecraft.loot.LootTables;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.math.VerticalSurfaceType;
+import net.minecraft.util.math.intprovider.ConstantIntProvider;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeEffects;
+import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.decorator.CaveSurfaceDecoratorConfig;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
+import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
+import org.jetbrains.annotations.NotNull;
 
 import static com.frostwizard4.Neutrino.SoundRegister.*;
+import static net.minecraft.world.gen.feature.ConfiguredFeatures.MOSS_VEGETATION;
 
 
 public class NeutrinoMain implements ModInitializer {
 
     public static final Block HALF_FULL_BOOKSHELF = new Block(FabricBlockSettings.of(Material.WOOD).strength(1.5F, 1.5F).sounds(BlockSoundGroup.WOOD).breakByTool(FabricToolTags.AXES));
-    /*
+
     public static final GlassDoor GLASS_DOOR = new GlassDoor(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 0.3f).sounds(BlockSoundGroup.GLASS).nonOpaque());
     public static final GlassTrapDoor GLASS_TRAPDOOR = new GlassTrapDoor(FabricBlockSettings.of(Material.GLASS).strength(0.3f, 0.3f).sounds(BlockSoundGroup.GLASS).nonOpaque());
-    */ //TODO Fix to be only Client side
+
     public static final ItemGroup NEUTRINO_GROUP = FabricItemGroupBuilder.create(new Identifier("neutrino", "neutrino_group")).icon(() -> new ItemStack(HALF_FULL_BOOKSHELF)).build();
     public static final ItemGroup NEUTRINO_DUNGEONS_GROUP = FabricItemGroupBuilder.create(new Identifier("neutrino", "neutrino_dungeons_group")).icon(() -> new ItemStack(NeutrinoMain.LIGHTNING_ROD_ARTIFACT)).build();
 
@@ -63,25 +85,16 @@ public class NeutrinoMain implements ModInitializer {
     public static final SoulPouchItem SOUL_POUCH = new SoulPouchItem(new FabricItemSettings().group(NEUTRINO_GROUP).rarity(Rarity.EPIC));
     public static final GoatHorn GOAT_HORN = new GoatHorn(new FabricItemSettings().group(NEUTRINO_GROUP).rarity(Rarity.COMMON));
 
-    /*
-    public void checkIfClient(World world) {
-        if(world.isClient()) {
-            BlockRenderLayerMap.INSTANCE.putBlock(GLASS_DOOR, RenderLayer.getCutout());
-            BlockRenderLayerMap.INSTANCE.putBlock(GLASS_TRAPDOOR, RenderLayer.getCutout());
-            FabricModelPredicateProviderRegistry.register(NeutrinoMain.SOUL_POUCH, new Identifier("filled"), (stack, world, entity, seed) -> {if (entity != null) {if (((PlayerEntityAccess) entity).neutrino$getSoulPouchCount() == 3000) {return 1.0f;} else {return 0.0f;}} else {return 0;}});
-        }
-    }*/ //TODO Fix to be only Client side
-    /*public static final Block DUST_BLOCK = new MossBlock(FabricBlockSettings.of(Material.MOSS_BLOCK).strength(0.3f, 0.3f).sounds(BlockSoundGroup.MOSS_BLOCK));
+    public static final Block DUST_BLOCK = new MossBlock(FabricBlockSettings.of(Material.MOSS_BLOCK).strength(0.3f, 0.3f).sounds(BlockSoundGroup.MOSS_BLOCK));
     private static <FC extends FeatureConfig> ConfiguredFeature<FC, ?> register(String id, ConfiguredFeature<FC, ?> configuredFeature) {
         return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier("neutrino", id), configuredFeature);
     }
     private static final ConfiguredSurfaceBuilder<TernarySurfaceConfig> DUST_CAVE_BUILDER = SurfaceBuilder.DEFAULT.withConfig(SurfaceBuilder.GRASS_CONFIG);
-    public static final RangeDecoratorConfig BOTTOM_TO_60 = new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.getBottom(), YOffset.fixed(60)));
     public static final RegistryKey<Biome> DUST_CAVE_KEY = RegistryKey.of(Registry.BIOME_KEY, new Identifier("neutrino", "dust_cave"));
 
 
     public static final ConfiguredFeature<VegetationPatchFeatureConfig, ?> DUST_PATCH = register("dust_patch", Feature.VEGETATION_PATCH.configure(new VegetationPatchFeatureConfig(BlockTags.MOSS_REPLACEABLE.getId(), new SimpleBlockStateProvider(DUST_BLOCK.getDefaultState()), () -> MOSS_VEGETATION, VerticalSurfaceType.FLOOR, ConstantIntProvider.create(1), 0.0F, 5, 0.8F, UniformIntProvider.create(4, 7), 0.3F)));
-    public static final ConfiguredFeature<?, ?> DUST_CAVES_VEGETATION = register("dust_caves_vegetation", DUST_PATCH.decorate(Decorator.CAVE_SURFACE.configure(new CaveSurfaceDecoratorConfig(VerticalSurfaceType.FLOOR, 12)).range(NeutrinoMain.BOTTOM_TO_60)).spreadHorizontally()).repeat(40);
+    public static final ConfiguredFeature<?, ?> DUST_CAVES_VEGETATION = register("dust_caves_vegetation", DUST_PATCH.decorate(Decorator.CAVE_SURFACE.configure(new CaveSurfaceDecoratorConfig(VerticalSurfaceType.FLOOR, 12)).range(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.getBottom(), YOffset.fixed(60))))).spreadHorizontally()).repeat(40);
 
     public static void addDustCavesDecoration(GenerationSettings.@NotNull Builder builder) {
         builder.feature(GenerationStep.Feature.VEGETAL_DECORATION, ConfiguredFeatures.LUSH_CAVES_CEILING_VEGETATION);
@@ -125,7 +138,7 @@ public class NeutrinoMain implements ModInitializer {
                 .spawnSettings(builder.build())
                 .generationSettings(builder2.build())
                 .build();
-    }*/ //Dust Cave Code
+    }//Dust Cave Code
 
     //TODO, add Invisible Item Frames
     //TODO, add more items from Minecraft Dungeons
@@ -136,13 +149,12 @@ public class NeutrinoMain implements ModInitializer {
         Registry.register(Registry.BLOCK, new Identifier("neutrino", "half_full_bookshelf"), HALF_FULL_BOOKSHELF);
         Registry.register(Registry.ITEM, new Identifier("neutrino", "half_full_bookshelf"), new BlockItem(HALF_FULL_BOOKSHELF, new FabricItemSettings().group(NEUTRINO_GROUP)));
 
-        /*
+
         Registry.register(Registry.ITEM, new Identifier("neutrino", "glass_door"), new BlockItem(GLASS_DOOR, new FabricItemSettings().group(NEUTRINO_GROUP)));
         Registry.register(Registry.BLOCK, new Identifier("neutrino", "glass_door"), GLASS_DOOR);
 
         Registry.register(Registry.ITEM, new Identifier("neutrino", "glass_trapdoor"), new BlockItem(GLASS_TRAPDOOR, new FabricItemSettings().group(NEUTRINO_GROUP)));
         Registry.register(Registry.BLOCK, new Identifier("neutrino", "glass_trapdoor"), GLASS_TRAPDOOR);
-        */ //TODO Fix to be only Client side
         Registry.register(Registry.ITEM, new Identifier("neutrino", "dirt_slab"), new BlockItem(DIRT_SLAB, new FabricItemSettings().group(NEUTRINO_GROUP)));
         Registry.register(Registry.BLOCK, new Identifier("neutrino", "dirt_slab"), DIRT_SLAB);
 
@@ -154,10 +166,10 @@ public class NeutrinoMain implements ModInitializer {
 
         Registry.register(Registry.ITEM, new Identifier("neutrino", "crafting_slab"), new BlockItem(CRAFTING_SLAB, new FabricItemSettings().group(NEUTRINO_GROUP)));
         Registry.register(Registry.BLOCK, new Identifier("neutrino", "crafting_slab"), CRAFTING_SLAB);
-        /*
+
         Registry.register(Registry.ITEM, new Identifier("neutrino", "dust_block"), new BlockItem(DUST_BLOCK, new FabricItemSettings().group(NEUTRINO_GROUP)));
         Registry.register(Registry.BLOCK, new Identifier("neutrino", "dust_block"), DUST_BLOCK);
-        */
+
         Registry.register(Registry.ITEM, new Identifier("neutrino", "backstabber"), BACKSTABBER);
         Registry.register(Registry.ITEM, new Identifier("neutrino", "enchanters_tome"), ENCHANTERS_TOME);
         Registry.register(Registry.ITEM, new Identifier("neutrino", "harvester"), HARVESTER);
@@ -167,10 +179,9 @@ public class NeutrinoMain implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("neutrino", "soul_healer"), SOUL_HEALER);
         Registry.register(Registry.ITEM, new Identifier("neutrino", "soul_pouch"), SOUL_POUCH);
         Registry.register(Registry.ITEM, new Identifier("neutrino", "goat_horn"), GOAT_HORN);
-        /*
+
         Registry.register(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, new Identifier("neutrino", "dust_cave"), DUST_CAVE_BUILDER);
         Registry.register(BuiltinRegistries.BIOME, DUST_CAVE_KEY.getValue(), DUST_CAVE);
-        */
 
         LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, table, setter) -> {
             if (LootTables.BASTION_TREASURE_CHEST.equals(id)) {
@@ -236,10 +247,10 @@ public class NeutrinoMain implements ModInitializer {
                 table.pool(poolBuilder);
             }
         });
-        /*
+
         OverworldBiomes.addContinentalBiome(DUST_CAVE_KEY, OverworldClimate.TEMPERATE, 2D);
         OverworldBiomes.addContinentalBiome(DUST_CAVE_KEY, OverworldClimate.COOL, 2D);
-        */
+
         Registry.register(Registry.SOUND_EVENT, ENCHANTERS_TOME_ACTIVATE_ID, ENCHANTERS_TOME_ACTIVATE);
         Registry.register(Registry.SOUND_EVENT, DAGGER_STAB_ID, DAGGER_STAB);
         Registry.register(Registry.SOUND_EVENT, HARVESTER_ACTIVATE_ID, HARVESTER_ACTIVATE);
