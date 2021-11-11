866e6fc58187ecf0266b12a6036803f324dc087f
FrostWizard4 <frostwizard4@gmail.com>
2021-11-11 17:49:21 -0600

    Revert "Merge pull request #20 from FrostWizard4/1.18"
    This reverts commit 00e20da80d491c75ee7ebe2b7e305fd3b359e1fc, reversing
    changes made to f94d5547d009c32027b55924591f985905fd8dbf.

--- src/main/java/com/frostwizard4/Neutrino/mixin/DesertTempleMixin.java
+++ src/main/java/com/frostwizard4/Neutrino/mixin/DesertTempleMixin.java
@@ -2,8 +2,8 @@ package com.frostwizard4.Neutrino.mixin;
 
 import com.frostwizard4.Neutrino.registry.BlockRegistry;
 import net.minecraft.structure.DesertTempleGenerator;
-import net.minecraft.structure.ShiftableStructurePiece;
 import net.minecraft.structure.StructurePieceType;
+import net.minecraft.structure.StructurePieceWithDimensions;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
@@ -14,19 +14,18 @@ import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
 @Mixin(DesertTempleGenerator.class)
-public abstract class DesertTempleMixin extends ShiftableStructurePiece {
+public abstract class DesertTempleMixin extends StructurePieceWithDimensions {
     protected DesertTempleMixin(StructurePieceType type, int x, int y, int z, int width, int height, int depth, Direction orientation) {
         super(type, x, y, z, width, height, depth, orientation);
     }
 
     @Inject(at = @At("RETURN"), method = "generate")
-    public void addTerracottaPot(StructureWorldAccess world, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, Random random, BlockBox boundingBox, ChunkPos chunkPos, BlockPos pos, CallbackInfo ci) {
+    public void addTerracottaPot(StructureWorldAccess world, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, Random random, BlockBox boundingBox, ChunkPos chunkPos, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         this.addBlock(world, BlockRegistry.DUNGEONS_POT.getDefaultState(), 5, 1, 10, boundingBox);
         this.addBlock(world, BlockRegistry.DUNGEONS_POT.getDefaultState(), 15, 1, 10, boundingBox);
         this.addBlock(world, BlockRegistry.DUNGEONS_POT.getDefaultState(), 10, 1, 17, boundingBox);
--- src/main/java/com/frostwizard4/Neutrino/mixin/SwampHutMixin.java
+++ src/main/java/com/frostwizard4/Neutrino/mixin/SwampHutMixin.java
@@ -1,8 +1,8 @@
 package com.frostwizard4.Neutrino.mixin;
 
 import com.frostwizard4.Neutrino.registry.BlockRegistry;
-import net.minecraft.structure.ShiftableStructurePiece;
 import net.minecraft.structure.StructurePieceType;
+import net.minecraft.structure.StructurePieceWithDimensions;
 import net.minecraft.structure.SwampHutGenerator;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
@@ -14,19 +14,18 @@ import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
 @Mixin(SwampHutGenerator.class)
-public abstract class SwampHutMixin extends ShiftableStructurePiece {
+public abstract class SwampHutMixin extends StructurePieceWithDimensions {
     protected SwampHutMixin(StructurePieceType type, int x, int y, int z, int width, int height, int depth, Direction orientation) {
         super(type, x, y, z, width, height, depth, orientation);
     }
 
     @Inject(at = @At("RETURN"), method = "generate")
-    public void neutrino$addDaturaFlower(StructureWorldAccess world, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, Random random, BlockBox boundingBox, ChunkPos chunkPos, BlockPos pos, CallbackInfo ci) {
+    public void neutrino$addDaturaFlower(StructureWorldAccess world, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, Random random, BlockBox boundingBox, ChunkPos chunkPos, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         this.addBlock(world, BlockRegistry.DATURA.getDefaultState(), 2, 2, 0, boundingBox);
     }
 }
