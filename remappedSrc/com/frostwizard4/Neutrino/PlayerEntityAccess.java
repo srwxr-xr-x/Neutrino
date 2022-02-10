f5338de570e3933f86de2bbff58a2e58ab1443f4
FrostWizard4 <88950263+FrostWizard4@users.noreply.github.com>
2022-02-10 11:33:30 -0600

    Delete remappedSrc/com/frostwizard4/Neutrino directory

--- remappedSrc/com/frostwizard4/Neutrino/PlayerEntityAccess.java
+++
@@ -1,8 +0,0 @@
-package com.frostwizard4.Neutrino;
-
-public interface PlayerEntityAccess {
-    float neutrino$getPowerCount();
-    void neutrino$setPowerCount(float newValue);
-    float neutrino$getSoulPouchCount();
-    void neutrino$setSoulPouchCount(float newValue);
-}
--- remappedSrc/com/frostwizard4/Neutrino/blocks/GlassDoor.java
+++
@@ -1,9 +0,0 @@
-package com.frostwizard4.Neutrino.blocks;
-
-import net.minecraft.block.DoorBlock;
-
-public class GlassDoor extends DoorBlock {
-    public GlassDoor(Settings settings) {
-        super(settings);
-    }
-}
